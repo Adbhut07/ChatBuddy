9253defbedfa14f9d943e601873d99d7c20625a3
Adbhut <adbhut.rei.2233@gmail.com>
2024-02-15 21:05:06 +0530

    finalized backend before adding socket.io

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,13 @@
+import User from '../models/user.model.js'
+export const getUsersForSidebar = async (req,res)=>{
+    try{
+        const loggedInUserId = req.user._id;
+
+        const filteredUsers = await User.find({_id: { $ne: loggedInUserId}}).select("-password");
+
+        res.status(200).json(filteredUsers);
+    } catch(error){
+        console.error("Erro in getUserForSidebar: ", error.message);
+        res.status(500),json({error: "Internal Server Error"});
+    }
+}
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+
 import authRoutes from './routes/auth.routes.js';
+import messageRoutes from './routes/message.routes.js';
+import userRoutes from "./routes/user.routes.js"
 import connectToMongoDB from "./db/db.js"
 
 const app = express();
@@ -8,7 +12,11 @@ dotenv.config();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cookieParser());
+
 app.use("/api/auth",authRoutes);
+app.use("/api/messages",messageRoutes);
+app.use("/api/users", userRoutes);
 
 // app.get("/",(req,res)=>{
 //     res.send("Hello");
