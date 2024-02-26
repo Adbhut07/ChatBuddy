985214b5e53a0e4e8d7e9cf4a2f4bb21de4b0bda
Adbhut Satsangi <125047511+Adbhut07@users.noreply.github.com>
2024-02-26 19:54:43 +0530

    socket url updated

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
     useEffect(() => {
         if(authUser){
-            const socket = io("http://localhost:3000",{
+            const socket = io("https://chatbuddy-xrxj.onrender.com",{
                 query:{
                     userId: authUser._id
                 }
